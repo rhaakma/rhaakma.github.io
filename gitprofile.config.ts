476eee6712f4f1e0fae91b65116d0bcd00eed867
Rachel Haakma <rhaakma7@gmail.com>
2025-04-30 09:19:27 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,9 +33,9 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Species Assessement Report for the California Newt',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'The Species Assessment Report provides an in-depth analysis of the California Newt species.  It provides information about the ecoloy, life history, population trends, and threats that all contribute to its vulnerable status. ',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
